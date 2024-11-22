3b0212b71bd45e0f6a9ece9a3c9f24ccebf1a21d
WaqarAhmad321 <codewithwaqarahmad@gmail.com>
2024-11-22 13:41:23 +0500

    update

--- src/app/(home)/_components/discover-button.tsx
+++ src/app/(home)/_components/discover-button.tsx
@@ -9,7 +9,8 @@ const DiscoverButton = () => {
       onClick={() => {
         window.scrollTo({
           behavior: "smooth",
-          top: document.getElementById("products-section")?.offsetTop - 100
+          top:
+            (document.getElementById("products-section")?.offsetTop ?? 0) - 100,
         });
       }}
       className="group flex items-center space-x-3 bg-red-600 text-white px-8 py-4 rounded-lg hover:bg-red-700 transition-colors">

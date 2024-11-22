0485686eb3c1e76f6b98c6b3ef6e2bbc148aa498
WaqarAhmad321 <codewithwaqarahmad@gmail.com>
2024-11-22 13:49:59 +0500

    updated

--- src/app/(home)/_components/discover-button.tsx
+++ src/app/(home)/_components/discover-button.tsx
@@ -4,6 +4,7 @@ import { ArrowRight } from "lucide-react";
 import React from "react";
 
 const DiscoverButton = () => {
+
   return (
     <button
       onClick={() => {
@@ -15,7 +16,7 @@ const DiscoverButton = () => {
       }}
       className="group flex items-center space-x-3 bg-red-600 text-white px-8 py-4 rounded-lg hover:bg-red-700 transition-colors">
       <span className="text-lg">Shop Collection</span>
-      <ArrowRight className="w-5 h-5 transform group-hover:translate-x-1 transition-transform" />
+      <ArrowRight className="w-5 h-5 transfor group-hover:translate-x-1 transition-transform" />
     </button>
   );
 };

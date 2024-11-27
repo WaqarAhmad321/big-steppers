570c97214b3302a4d3f3d9f48fafa08d72da2c22
WaqarAhmad321 <codewithwaqarahmad@gmail.com>
2024-11-28 03:40:20 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import React from "react";
+import React, { Suspense } from "react";
 
 import HeroSection from "./(home)/_components/hero-section";
 import FeaturesSection from "./(home)/_components/features-section";
 import ProductsSection from "./(home)/_components/products-section";
-
 function App() {
   return (
     <div className="min-h-screen bg-white font-body">
@@ -11,7 +10,9 @@ function App() {
 
       <FeaturesSection />
 
-      <ProductsSection />
+      <Suspense fallback={<h1>Loading..</h1>}>
+        <ProductsSection />
+      </Suspense>
     </div>
   );
 }

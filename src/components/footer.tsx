2d80f5034f503835e1860d8215ca2baed09b68f4
WaqarAhmad321 <codewithwaqarahmad@gmail.com>
2025-01-05 22:33:42 +0500

    Fix footer issue

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "next/link";
 import { contactLinks, quickLinks, socialLinks } from "@/lib/links";
 
-export default function Footer() {z
+export default function Footer() {
   return (
     <footer className="bg-gray-900 text-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
